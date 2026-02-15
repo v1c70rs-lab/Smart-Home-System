e770d9cf00491a5a6dee04aeb624b797c74032bc
Victor Payá Sánchez <victorpayasanchez@gmail.com>
2026-02-15 18:54:06 +0100

    Update broker.py

--- broker.py
+++ broker.py
@@ -10,13 +10,59 @@ from pysolar.solar import get_altitude
 from astral import LocationInfo
 
 shabbat_dict = [
- "13-dec 16:01",
- "20-dec 16:03",
- "27-dec 16:08",
- "03-jan 16:15",
- "10-jan 16:25",
+    "02-jan 16:21",
+    "09-jan 16:35",
+    "16-jan 16:48",
+    "23-jan 17:01",
+    "30-jan 17:13",
+    "06-feb 17:25",
+    "13-feb 17:36",
+    "20-feb 17:47",
+    "27-feb 17:58",
+    "06-mar 18:09",
+    "13-mar 18:20",
+    "20-mar 18:31",
+    "27-mar 18:42",
+    "03-apr 18:52",
+    "10-apr 19:01",
+    "17-apr 19:10",
+    "24-apr 19:18",
+    "01-may 19:26",
+    "08-may 19:34",
+    "15-may 19:42",
+    "22-may 19:50",
+    "29-may 19:57",
+    "05-jun 20:04",
+    "12-jun 20:10",
+    "19-jun 20:15",
+    "26-jun 20:19",
+    "03-jul 20:21",
+    "10-jul 20:21",
+    "17-jul 20:18",
+    "24-jul 20:11",
+    "31-jul 20:01",
+    "07-aug 19:49",
+    "14-aug 19:36",
+    "21-aug 19:23",
+    "28-aug 19:08",
+    "04-sep 18:52",
+    "11-sep 18:36",
+    "18-sep 18:20",
+    "25-sep 18:03",
+    "02-oct 17:47",
+    "09-oct 17:31",
+    "16-oct 17:14",
+    "23-oct 16:58",
+    "30-oct 16:42",
+    "06-nov 16:27",
+    "13-nov 16:14",
+    "20-nov 16:03",
+    "27-nov 15:55",
+    "04-dec 15:51",
+    "11-dec 15:51",
+    "18-dec 15:55",
+    "25-dec 16:02"
 ]
-
 
 # logger instellingen
 logger = logging.getLogger()
@@ -40,7 +86,7 @@ rotating_handler = RotatingFileHandler(
     backupCount=3)
 rotating_handler.setLevel("DEBUG")
 rotating_handler.setFormatter(formatter)
-logger.addHandler(rotating_handler)
+#logger.addHandler(rotating_handler)
 
 # MQTT instellingen
 MQTT_BROKER = "localhost"
@@ -65,59 +111,60 @@ sun = Sun(latitude, longtitude)
 
 # Globale variabelen
 light_value = 150
-lampen_status = {"normaal": False, "shabbat": False, "warmhoudplaat": False}
+lampen_status = {"normaal": False, "shabbat": False, "warmhoudplaat": False, "warmhoudplaat2": False}
 timer = {"normaal": 0, "shabbat": 0,}
 
 # MQTT callback functies
 def on_message(client, userdata, message):
     global light_value
     light_value = int(message.payload.decode())
-    print(f"Ontvangen lichtwaarde: {light_value}")
+    logger.info(f"Ontvangen lichtwaarde: {light_value}")
 
 def on_connect(client, userdata, flags, rc):
-    print(f"Verbonden met de broker! Code: {rc}")
+    logger.info(f"Verbonden met de broker! Code: {rc}")
     client.subscribe(MQTT_TOPIC_LIGHT)
 
 # Bereken normale tijdvakken
-def bereken_tijdvakken():
-    begin_terne = datetime.now().date().replace(month=1, day=1)
-    eind_terne = datetime.now().date().replace(month=3, day=20)
+def calculate_time_window():
+    current_date_time = datetime.now()
+    current_date_time_tz = tz_netherlands.localize(current_date_time)
+    start_date = current_date_time.date().replace(month=1, day=1)
+    end_date = current_date_time.date().replace(month=3, day=20)
     
-    if begin_terne <= datetime.now().date() <= eind_terne:
-        huidige_datum = datetime.now(tz_netherlands) #+ timedelta(days=1) # bug die er voor zorgt dat get_sunset_time de dag ervoor pakt i.p.v. de huidige dag, vandaar dat we een dag optellen bij de huidige_datum. Bug gebeurt alleen na ongeveer 21 maart. Dus zomer- wintertijd gerelateerd waarschijnlijk
+    if start_date <= current_date_time.date() <= end_date:
+        current_date = current_date_time_tz #+ timedelta(days=1) # bug die er voor zorgt dat get_sunset_time de dag ervoor pakt i.p.v. de huidige dag, vandaar dat we een dag optellen bij de huidige_datum. Bug gebeurt alleen na ongeveer 21 maart. Dus zomer- wintertijd gerelateerd waarschijnlijk
     else:
-        huidige_datum = datetime.now(tz_netherlands) + timedelta(days=1) # bug die er voor zorgt dat get_sunset_time de dag ervoor pakt i.p.v. de huidige dag, vandaar dat we een dag optellen bij de huidige_datum. Bug gebeurt alleen na ongeveer 21 maart. Dus zomer- wintertijd gerelateerd waarschijnlijk
+        current_date = current_date_time_tz + timedelta(days=1) # bug die er voor zorgt dat get_sunset_time de dag ervoor pakt i.p.v. de huidige dag, vandaar dat we een dag optellen bij de huidige_datum. Bug gebeurt alleen na ongeveer 21 maart. Dus zomer- wintertijd gerelateerd waarschijnlijk
     
-    today_ss = sun.get_sunset_time(huidige_datum)
-    begin_tijd = today_ss.astimezone(tz_netherlands) - timedelta(hours=1)
-    eind_tijd = today_ss.astimezone(tz_netherlands).replace(hour=22, minute=30, second=0)
-    return begin_tijd, eind_tijd
+    today_ss = sun.get_sunset_time(current_date)
+    start_time = today_ss.astimezone(tz_netherlands) - timedelta(hours=1)
+    end_time = tz_netherlands.localize(current_date_time).replace(hour=22, minute=30, second=0)
+    return start_time, end_time
 
 # Bereken Shabbat tijdvakken
-def shabbat_bereken_tijdvakken():
-    current_date = datetime.now(tz_netherlands).date()
+def shabbat_calculate_time_window():
+    current_date_tz = datetime.now(tz_netherlands).date()
     for x in shabbat_dict:
-        print("\n",x,"\n")
         parsed_date = datetime.strptime(x, "%d-%b %H:%M")
         parsed_date = parsed_date.replace(year=datetime.now().year)
         parsed_date = tz_netherlands.localize(parsed_date)
-        print(parsed_date)
-        if parsed_date.date() == current_date:
-            shabbat_begin_tijd = parsed_date
-            shabbat_eind_tijd = parsed_date + timedelta(hours=1, minutes=30)
-            return shabbat_begin_tijd, shabbat_eind_tijd
-        # placeholder tijd retourneren die nooit op shabbat valt, want het is toch geen shabbat
-    placeholder = tz_netherlands.localize(datetime(2025, 12, 31, 0, 0, 0))
+        if parsed_date.date() == current_date_tz:
+            shabbat_start_time = parsed_date
+            shabbat_end_time = parsed_date + timedelta(hours=1, minutes=30)
+            return shabbat_start_time, shabbat_end_time
+        # placeholder tijd retourneren want het is toch geen shabbat
+    placeholder = tz_netherlands.localize(datetime(2024, 11, 30, 0, 0, 0))
     return placeholder, placeholder
 
 # Bereken warmhoudplaat tijdvakken
-def warmhoudplaat_bereken_tijdvakken():
-    warmhoudplaat_begin_tijd = datetime.now(tz_netherlands).replace(hour=12, minute=0, second=0)
-    warmhoudplaat_eind_tijd = datetime.now(tz_netherlands).replace(hour=13, minute=0, second=0)
-    return warmhoudplaat_begin_tijd, warmhoudplaat_eind_tijd
+def warmhoudplaat_calculate_time_window():
+    current_date_time_tz = datetime.now(tz_netherlands)
+    warmhoudplaat_start_time = current_date_time_tz.replace(hour=12, minute=0, second=0)
+    warmhoudplaat_end_time = current_date_time_tz.replace(hour=13, minute=0, second=0)
+    return warmhoudplaat_start_time, warmhoudplaat_end_time
 
 # Beheer lampen
-def beheer_lamp(lamp, begin_tijd, eind_tijd, topicList, licht_drempel=None, dag_restrictie=None, seizoensperiode=None):
+def manage_devices(lamp, begin_tijd, eind_tijd, topicList, licht_drempel=None, dag_restrictie=None, seizoensperiode=None):
     huidige_tijd = datetime.now(tz_netherlands)
     global lampen_status, timer
 
@@ -162,8 +209,7 @@ def beheer_lamp(lamp, begin_tijd, eind_tijd, topicList, licht_drempel=None, dag_
         if not lampen_status[lamp] and licht_drempel:
             logger.debug(f"{lamp.capitalize()} is nog niet aan en licht_drempel is meegegeven")
             if light_value < licht_drempel:
-                timer[lamp] += 1
-                print(f"{lamp.capitalize()}-timer: {timer[lamp]} seconden")
+                timer[lamp] += 10
                 logger.debug(f"{lamp.capitalize()}-timer loopt: {timer[lamp]} seconden")
                 if timer[lamp] >= 60:
                     for topic in topicList:
@@ -213,28 +259,29 @@ client.connect(MQTT_BROKER, MQTT_PORT, 60)
 client.loop_start()
 
 # Hoofdprogramma
-begin_tijd, eind_tijd = bereken_tijdvakken()
-shabbat_begin_tijd, shabbat_eind_tijd = shabbat_bereken_tijdvakken()
-warmhoudplaat_begin_tijd, warmhoudplaat_eind_tijd = warmhoudplaat_bereken_tijdvakken()
+begin_tijd, eind_tijd = calculate_time_window()
+shabbat_begin_tijd, shabbat_eind_tijd = shabbat_calculate_time_window()
+warmhoudplaat_begin_tijd, warmhoudplaat_eind_tijd = warmhoudplaat_calculate_time_window()
 while True:
     huidige_tijd = datetime.now(tz_netherlands)
 
     # Update tijdvakken bij een nieuwe dag
     if huidige_tijd.date() != begin_tijd.date():
         logger.debug("Nieuwe dag aangebroken, bereken nieuwe tijdvakken!")
-        begin_tijd, eind_tijd = bereken_tijdvakken()
-        shabbat_begin_tijd, shabbat_eind_tijd = shabbat_bereken_tijdvakken()
-        warmhoudplaat_begin_tijd, warmhoudplaat_eind_tijd = warmhoudplaat_bereken_tijdvakken()
+        begin_tijd, eind_tijd = calculate_time_window()
+        shabbat_begin_tijd, shabbat_eind_tijd = shabbat_calculate_time_window()
+        warmhoudplaat_begin_tijd, warmhoudplaat_eind_tijd = warmhoudplaat_calculate_time_window()
 
-    print(f"Huidige tijd: {huidige_tijd}")
-    print(f"Normaal-tijdvak: {begin_tijd} - {eind_tijd}")
-    print(f"Shabbat-tijdvak: {shabbat_begin_tijd} - {shabbat_eind_tijd}")
-    print(f"Warmhoudplaat-tijdvak: {warmhoudplaat_begin_tijd} - {warmhoudplaat_eind_tijd}")
-    print(f"\n{lampen_status}\n")
+    logger.info(f"Huidige tijd: {huidige_tijd}")
+    logger.info(f"Normaal-tijdvak: {begin_tijd} - {eind_tijd}")
+    logger.info(f"Shabbat-tijdvak: {shabbat_begin_tijd} - {shabbat_eind_tijd}")
+    logger.info(f"Warmhoudplaat-tijdvak: {warmhoudplaat_begin_tijd} - {warmhoudplaat_eind_tijd}")
+    logger.info(f"{lampen_status}")
 
     # Beheer lampen
-    beheer_lamp("normaal", begin_tijd, eind_tijd, normaalTopicList, 50)
-    beheer_lamp("shabbat", shabbat_begin_tijd, shabbat_eind_tijd, shabbatTopicList, 100, dag_restrictie=4)
-    beheer_lamp("warmhoudplaat", warmhoudplaat_begin_tijd, warmhoudplaat_eind_tijd, warmhoudplaatTopicList, dag_restrictie=5, seizoensperiode=("21-03-2024", "21-09-2024"))
+    manage_devices("normaal", begin_tijd, eind_tijd, normaalTopicList, 45)
+    manage_devices("shabbat", shabbat_begin_tijd, shabbat_eind_tijd, shabbatTopicList, 100, dag_restrictie=4)
+    manage_devices("warmhoudplaat2", shabbat_begin_tijd, shabbat_begin_tijd + timedelta(hours=1), warmhoudplaatTopicList, dag_restrictie=4)
+    manage_devices("warmhoudplaat", warmhoudplaat_begin_tijd, warmhoudplaat_eind_tijd, warmhoudplaatTopicList, dag_restrictie=5)
 
-    time.sleep(1)
+    time.sleep(10)
