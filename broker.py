2ca0c55cfa27ad7e1ea6188093864acbad7cbd8e
Victor <victorpayasanchez@gmail.com>
2026-03-11 20:18:50 +0100

    broker: create universal current_time variable

--- broker.py
+++ broker.py
@@ -117,6 +117,9 @@ longtitude = 4.9041
 tz_netherlands = pytz.timezone("Europe/Amsterdam")
 sun = Sun(latitude, longtitude)
 
+# Huidige tijd
+current_time = datetime.now(tz_netherlands)
+
 device_state = {"dagelijks": False, "shabbat": False, "warmhoudplaat": False, "warmhoudplaat2": False, "kachel": False}
 timer = {"dagelijks": 0, "shabbat": 0, "kachel": 0}
 
@@ -147,7 +150,7 @@ client.loop_start()
 
 # Dag restrictie check
 def f_day_restriction(device, topicList, day_restriction):
-    if day_restriction and huidige_tijd.weekday() != day_restriction:
+    if day_restriction and current_time.weekday() != day_restriction:
         logger.debug(f"\"{device.capitalize()}\" won't turn on, not the right day!")
         if device_state[device]:
             logger.debug(f"\"{device.capitalize()}\" goes off, not the right day!")
@@ -157,15 +160,15 @@ def f_day_restriction(device, topicList, day_restriction):
 
 # Seizoensperiode check
 def f_season_period(device, season_period):
-    start_datum1 = datetime.strptime(season_period[0], "%d-%m-%Y").replace(year=huidige_tijd.year)
+    start_datum1 = datetime.strptime(season_period[0], "%d-%m-%Y").replace(year=current_time.year)
     start_datum = tz_netherlands.localize(start_datum1)
-    eind_datum1 = datetime.strptime(season_period[1], "%d-%m-%Y").replace(year=huidige_tijd.year)
+    eind_datum1 = datetime.strptime(season_period[1], "%d-%m-%Y").replace(year=current_time.year)
     eind_datum = tz_netherlands.localize(eind_datum1)
     if start_datum > eind_datum:
         # Indien de periode over de jaarwisseling gaat (bijv. okt - mrt)
-        eind_datum = eind_datum.replace(year=huidige_tijd.year + 1) if huidige_tijd < start_datum else eind_datum
+        eind_datum = eind_datum.replace(year=current_time.year + 1) if current_time < start_datum else eind_datum
     
-    if not (start_datum <= huidige_tijd <= eind_datum): # Als de start_datum niet kleiner is dan huidige tijd en ook niet kleiner is dan eind_datum, dan zit de huidige_tijd niet binnen het seizoensvak
+    if not (start_datum <= current_time <= eind_datum): # Als de start_datum niet kleiner is dan huidige tijd en ook niet kleiner is dan eind_datum, dan zit de current_time niet binnen het seizoensvak
         logger.debug(f"{device.capitalize()} wordt niet ingeschakeld buiten de season_period ({start_datum1}, {eind_datum1}).")
         return False
     logger.debug(f"Juiste season_period voor {device}")
@@ -190,29 +193,26 @@ def powerStateDevices(device, topicList, state):
 
 # Bereken normale tijdvakken
 def calculate_time_window():
-    current_date_time = datetime.now()
-    current_date_time_tz = tz_netherlands.localize(current_date_time)
-    start_date = current_date_time.date().replace(month=1, day=1)
-    end_date = current_date_time.date().replace(month=3, day=20)
+    start_date = current_time.date().replace(month=1, day=1)
+    end_date = current_time.date().replace(month=3, day=20)
     
-    if start_date <= current_date_time.date() <= end_date:
-        current_date = current_date_time_tz #+ timedelta(days=1) # bug die er voor zorgt dat get_sunset_time de dag ervoor pakt i.p.v. de huidige dag, vandaar dat we een dag optellen bij de huidige_datum. Bug gebeurt alleen na ongeveer 21 maart. Dus zomer- wintertijd gerelateerd waarschijnlijk
+    if start_date <= current_time.date() <= end_date:
+        current_date = current_time #+ timedelta(days=1) # bug die er voor zorgt dat get_sunset_time de dag ervoor pakt i.p.v. de huidige dag, vandaar dat we een dag optellen bij de huidige_datum. Bug gebeurt alleen na ongeveer 21 maart. Dus zomer- wintertijd gerelateerd waarschijnlijk
     else:
-        current_date = current_date_time_tz + timedelta(days=1) # bug die er voor zorgt dat get_sunset_time de dag ervoor pakt i.p.v. de huidige dag, vandaar dat we een dag optellen bij de huidige_datum. Bug gebeurt alleen na ongeveer 21 maart. Dus zomer- wintertijd gerelateerd waarschijnlijk
+        current_date = current_time + timedelta(days=1) # bug die er voor zorgt dat get_sunset_time de dag ervoor pakt i.p.v. de huidige dag, vandaar dat we een dag optellen bij de huidige_datum. Bug gebeurt alleen na ongeveer 21 maart. Dus zomer- wintertijd gerelateerd waarschijnlijk
     
     today_ss = sun.get_sunset_time(current_date)
     start_time = today_ss.astimezone(tz_netherlands) - timedelta(hours=1)
-    end_time = tz_netherlands.localize(current_date_time).replace(hour=22, minute=30, second=0)
+    end_time = current_time.replace(hour=22, minute=30, second=0)
     return start_time, end_time
 
 # Bereken Shabbat tijdvakken
 def shabbat_calculate_time_window():
-    current_date_tz = datetime.now(tz_netherlands).date()
     for x in shabbat_dict:
         parsed_date = datetime.strptime(x, "%d-%b %H:%M")
         parsed_date = parsed_date.replace(year=datetime.now().year)
         parsed_date = tz_netherlands.localize(parsed_date)
-        if parsed_date.date() == current_date_tz:
+        if parsed_date.date() == current_time.date():
             shabbat_start_time = parsed_date
             shabbat_end_time = parsed_date + timedelta(hours=1, minutes=30)
             return shabbat_start_time, shabbat_end_time
@@ -222,28 +222,26 @@ def shabbat_calculate_time_window():
 
 # Bereken warmhoudplaat tijdvakken
 def warmhoudplaat_calculate_time_window():
-    current_date_time_tz = datetime.now(tz_netherlands)
-    warmhoudplaat_start_time = current_date_time_tz.replace(hour=12, minute=0, second=0)
-    warmhoudplaat_end_time = current_date_time_tz.replace(hour=13, minute=0, second=0)
+    warmhoudplaat_start_time = current_time.replace(hour=12, minute=0, second=0)
+    warmhoudplaat_end_time = current_time.replace(hour=13, minute=0, second=0)
     return warmhoudplaat_start_time, warmhoudplaat_end_time
 
 # Beheer deviceen
 def manage_devices(device, begin_tijd, eind_tijd, topicList, sens_value=None, threshold=None, day_restriction=None, season_period=None):
-    # huidige_tijd = datetime.now(tz_netherlands)
     global device_state, timer
 
     # Als season_period is meegegeven, check dan of het binnen het seizoen valt
     # Controleer season_period
     if season_period:
-        if not f_season_period(device, season_period):
+        if f_season_period(device, season_period) == False:
             return
 
     # Als dag regstrictie is meegegeven, check dan welke dag het is
     if day_restriction:
-        if not f_day_restriction(device, topicList, day_restriction):
+        if f_day_restriction(device, topicList, day_restriction) == False:
             return
 
-    if begin_tijd <= huidige_tijd <= eind_tijd:
+    if begin_tijd <= current_time <= eind_tijd:
         logger.debug(f"Current time is within {device}-time window")
         if threshold and sens_value < threshold and not device_state[device]:
             logger.debug(f"Sensor value ({sens_value}) is lower than threshold ({threshold})")
@@ -275,17 +273,17 @@ begin_tijd, eind_tijd = calculate_time_window()
 shabbat_begin_tijd, shabbat_eind_tijd = shabbat_calculate_time_window()
 warmhoudplaat_begin_tijd, warmhoudplaat_eind_tijd = warmhoudplaat_calculate_time_window()
 while True:
-    huidige_tijd = datetime.now(tz_netherlands)
+    current_time = datetime.now(tz_netherlands)
 
     # Update tijdvakken bij een nieuwe dag
-    if huidige_tijd.date() != begin_tijd.date():
+    if current_time.date() != begin_tijd.date():
         logger.debug("Nieuwe dag aangebroken, bereken nieuwe tijdvakken!")
         begin_tijd, eind_tijd = calculate_time_window()
         shabbat_begin_tijd, shabbat_eind_tijd = shabbat_calculate_time_window()
         warmhoudplaat_begin_tijd, warmhoudplaat_eind_tijd = warmhoudplaat_calculate_time_window()
 
     print("")
-    logger.info(f"Current time: {huidige_tijd}")
+    logger.info(f"Current time: {current_time}")
     logger.info(f"Dagelijks-time window: {begin_tijd} - {eind_tijd}")
     logger.info(f"Shabbat-time window: {shabbat_begin_tijd} - {shabbat_eind_tijd}")
     logger.info(f"Warmhoudplaat-time window: {warmhoudplaat_begin_tijd} - {warmhoudplaat_eind_tijd}")
